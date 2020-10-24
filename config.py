7ee275a0722f4aafc0d92f100d5241d33bfbac37
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-24 10:59:51 +0330

    a little bit of error handling for config parsing

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import yaml
 from house.room import Room,Action,Actions
 from house.cycle import Cycle
 from house.house import House
+import logging as logger
 
 
 def parse_config(path):
@@ -20,13 +21,31 @@ def get_rooms(path):
     rooms=[]
     for r in room_paths:
         cfg=parse_config(os.path.join(path, r))
+        cfg=(cfg,{})[cfg==None]
+######################################## Pattern ############################################
+        if not "pattern"in cfg:cfg["pattern"]=".*"
+
+######################################## Actions ############################################
         pre,post,fail=None,None,None
-        if "pre_script" in cfg["actions"]: pre=Action(cfg["actions"]["pre_script"])
-        if "post_script" in cfg["actions"]: post=Action(cfg["actions"]["post_script"])
-        if "on_fail_script" in cfg["actions"]: fail=Action(cfg["actions"]["on_fail_script"])
+        if "actions" in cfg:
+            if "pre_script" in cfg["actions"]: pre=Action(cfg["actions"]["pre_script"])
+            if "post_script" in cfg["actions"]: post=Action(cfg["actions"]["post_script"])
+            if "on_fail_script" in cfg["actions"]: fail=Action(cfg["actions"]["on_fail_script"])
         actions=Actions(pre,post,fail)
-        cycles=[Cycle[cycle] for cycle in cfg["cycles"]]
-        rooms.append(Room(r,cfg["path"],cfg["pattern"],cycles,actions))
+
+######################################## Cycles ############################################
+        cycles=[]
+        if "cycles" in cfg:
+            cycles=[Cycle[cycle] for cycle in cfg["cycles"]]
+        else:
+            cycles=[c for c in Cycle]
+            
+        try:
+            rooms.append(Room(r,cfg["path"],cfg["pattern"],cycles,actions))
+        except Exception:
+            logger.error(f"path must be specified for room:{r}")
+            raise SystemExit
+            
     return rooms
 
 config=""
