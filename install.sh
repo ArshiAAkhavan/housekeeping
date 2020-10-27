549bab9056b717a209b229e84b36286c7b7d7e4b
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-27 12:38:38 +0330

    writing install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 PYTHON_PATH=$(which python3)
 PWD=$(pwd)
 
-sed "1 i\\#!$PYTHON_PATH " housekeeping.py
+sed "1 i\\#!$PYTHON_PATH " -i housekeeping.py
 chmod +x housekeeping.py
 ln -s $PWD/housekeeping.py /usr/bin/housekeeping
 
