c5a3210acb728a4e64f1181fdde26f071363e617
Arshia Akhavan <letmemakenewone@gmail.com>
2020-10-27 10:06:55 +0330

    adding DAILY_M to cycles

--- house/cycle.py
+++ house/cycle.py
@@ -5,8 +5,9 @@ CycleTuple = namedtuple('CycleTuple', 'unit bound')
 
 hour = 3600
 class Cycle(Enum):
-    HOURLY = CycleTuple(hour*1, hour*24)
-    DAILY  = CycleTuple(hour*24, hour*24*7)
-    WEEKLY = CycleTuple(hour*24*7, hour*24*30)
+    HOURLY = CycleTuple(hour*1,     hour*24)
+    DAILY  = CycleTuple(hour*24,    hour*24*7)
+    DAILY_M= CycleTuple(hour*24,    hour*24*30)
+    WEEKLY = CycleTuple(hour*24*7,  hour*24*30)
     MONTHLY= CycleTuple(hour*24*30, hour*24*365)
     YEARLY = CycleTuple(hour*24*365,float("inf"))
